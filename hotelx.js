767987c0ae75ed59599cedf4424e8dcba2127c3a
abdelilleh <143023531+Moncif2005@users.noreply.github.com>
2023-09-07 21:01:47 +0100

    Add files via upload

---
+++ hotelx.js
@@ -0,0 +1,5 @@
+const humburugr = document.querySelector(".humburugr");
+humburugr.addEventListener("click", () => {
+  const navbar = document.querySelector(".nav-bar");
+  navbar.classList.toggle("active");
+});
